361affe93b91a15301e7b216c66d4b7adb94082a
Averlex <averlex2809@gmail.com>
2024-06-15 23:04:50 +0300

    User table added + linkage

--- webapp/models.py
+++ webapp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-# Create your models here.
+_MAX_NAME = 50
+_MAX_PHONE = 20
+_MAX_ADDRESS = 150
+_MAX_FEEDBACK = 500
+_MAX_LINK = 100
 
 
 class FAQ(models.Model):
@@ -17,12 +21,13 @@ class FAQ(models.Model):
         # Random ordering for FAQ table
         ordering = ['?']
 
+    pass
+
 
 class UserFeedback(models.Model):
     """
     User feedback table. Stores user scores, feedback text, as well as feedback sources and administration replies
     """
-    # link with user?
     _FEEDBACK_TYPE = {
         0: "Удобство заказа",
         1: "Функционал сайта",
@@ -39,14 +44,15 @@ class UserFeedback(models.Model):
     }
 
     rate = models.SmallIntegerField(default=0, db_default=0, blank=True, help_text='Пожалуйста, оцените работу сервиса')
-    text = models.TextField(default='', db_default='', help_text='Поделитесь впечатлениями о работе сервиса', blank=True, max_length=500)
-    reply = models.TextField(default='', db_default='', blank=True, max_length=500)
-    source_link = models.CharField(default='', db_default='', blank=True, max_length=100)
+    text = models.TextField(default='', db_default='', help_text='Поделитесь впечатлениями о работе сервиса', blank=True, max_length=_MAX_FEEDBACK)
+    reply = models.TextField(default='', db_default='', blank=True, max_length=_MAX_FEEDBACK)
+    source_link = models.CharField(default='', db_default='', blank=True, max_length=_MAX_LINK)
     feedback_type = models.SmallIntegerField(default=0, db_default=0, choices=_FEEDBACK_TYPE)
     feedback_source = models.SmallIntegerField(default=0, db_default=0, choices=_FEEDBACK_SOURCE)
+    user = models.ForeignKey("User", on_delete=models.CASCADE, null=True, blank=False, db_default=None)
 
     def __str__(self):
-        return self.rate
+        return self.rate.__str__() + ": " + self.text.__str__()
 
     class Meta:
         # Lower rates with details first
@@ -55,4 +61,36 @@ class UserFeedback(models.Model):
             models.CheckConstraint(check=models.Q(rate__gte=0) | models.Q(text__neq=''), name='empty_feedback')
         ]
 
+        pass
 
+    pass
+
+
+class User(models.Model):
+    """
+    Base user info
+    """
+    _USER_GROUP = {
+        0: "Администратор",
+        1: "Сотрудник магазина",
+        2: "Довольный клиент",
+    }
+
+    _DELIVERY_TYPE = {
+        0: "Самовывоз",
+        1: "СДЭК",
+    }
+
+    feedback_source = models.SmallIntegerField(default=2, db_default=2, choices=_USER_GROUP)
+    registration_date = models.DateField(auto_now_add=True, editable=False)
+    name = models.CharField(max_length=_MAX_NAME)
+    second_name = models.CharField(max_length=_MAX_NAME)
+    last_name = models.CharField(max_length=_MAX_NAME)
+    phone = models.CharField(max_length=_MAX_PHONE, help_text='Основной контактный номер')
+    email = models.EmailField(help_text='Действующий адрес электронной почты')
+    birthdate = models.DateField(blank=True)
+    pref_delivery_type = models.SmallIntegerField(default=0, db_default=0, blank=True, choices=_DELIVERY_TYPE)
+    main_address = models.CharField(max_length=_MAX_ADDRESS, blank=True, default='', db_default='')
+
+    def __str__(self):
+        return self.name.__str__() + " " + self.second_name.__str__() + " " + self.last_name.__str__()
